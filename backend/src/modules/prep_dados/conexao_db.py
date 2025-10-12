542ca44021e55d8714aab75915b5b1ef0caeb8d5
hribes <hribe.silva@gmail.com>
2025-10-12 00:06:52 -0300

    Ajuste de variaveis

--- backend/src/modules/prep_dados/conexao_db.py
+++ backend/src/modules/prep_dados/conexao_db.py
@@ -1,20 +1,19 @@
+import os
+from dotenv import load_dotenv
 import mysql.connector
 from mysql.connector import Error
 
+load_dotenv()
 def criar_conexao():
     """ Cria uma conexão com o banco de dados MySQL """
-    meu_host = "localhost"
-    meu_usuario = "root"
-    minha_senha = "Fec43867614."
-    meu_banco_de_dados = "fluxovital"
     
     conexao = None
     try:
         conexao = mysql.connector.connect(
-            host=meu_host,
-            user=meu_usuario,
-            password=minha_senha,
-            database=meu_banco_de_dados
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
         )
         print("Conexão com o banco de dados MySQL bem-sucedida!")
     except Error as e:
@@ -28,19 +27,3 @@ def ler_query_de_arquivo(caminho_arquivo):
         query = arquivo.read()
     return query
 
-# --- INÍCIO DO PROGRAMA PRINCIPAL ---
-
-conexao = criar_conexao()
-
-# Exemplo de como usar a conexão (opcional)
-if conexao is not None and conexao.is_connected():
-    # Você pode criar um cursor para executar queries SQL
-    cursor = conexao.cursor()
-    cursor.execute("SELECT database();")
-    record = cursor.fetchone()
-    print("Você está conectado ao banco de dados: ", record)
-
-    # Lembre-se de fechar a conexão e o cursor quando terminar
-    cursor.close()
-    conexao.close()
-    print("A conexão com o MySQL foi fechada.")
--- backend/src/modules/prep_dados/prep_dados.py
+++ backend/src/modules/prep_dados/prep_dados.py
@@ -1,6 +1,9 @@
+import os
+from dotenv import load_dotenv
 import pandas as pd
 from conexao_db import criar_conexao, ler_query_de_arquivo
 
+load_dotenv()
 conexao = criar_conexao()
 df_solicitacao_consulta = None
 df_veiculo = None
@@ -9,7 +12,7 @@ if conexao is not None and conexao.is_connected():
     cursor = conexao.cursor()
     
     # Busca da Data e hora da chegada e também dos endereços de busca e entrega
-    caminho_do_arquivo_sql_1 = r"C:\Users\heloi\OneDrive\Desktop\projeto-FluxoVital\backend\src\modules\queries\solicitacao_consulta.sql"
+    caminho_do_arquivo_sql_1 = os.getenv("CAMINHO_SQL_SOLICITACAOCONSULTAS")
     query_solicitacao_consulta = ler_query_de_arquivo(caminho_do_arquivo_sql_1)
     
     cursor.execute(query_solicitacao_consulta)
@@ -21,8 +24,8 @@ if conexao is not None and conexao.is_connected():
     else:
         print("A consulta não retornou resultados da resultados_query_solicitacao_consulta.")
 
-    # 
-    caminho_do_arquivo_sql_2 = r"C:\Users\heloi\OneDrive\Desktop\projeto-FluxoVital\backend\src\modules\queries\veiculos.sql"
+    # Busca as infos dos veículos
+    caminho_do_arquivo_sql_2 = os.getenv("CAMINHO_SQL_VEICULOS")
     query_veiculo = ler_query_de_arquivo(caminho_do_arquivo_sql_2)
     
     cursor.execute(query_veiculo)
